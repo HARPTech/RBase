0d8ea192686816347f9b2841091527ef2a3486d6
Max Heisinger <mail@maximaximal.com>
2017-08-13 16:00:01 +0200

    Document Movement Properties and change X&Y axis to int8.

--- include/RRegistry/Movement.hpp
+++ include/RRegistry/Movement.hpp
@@ -10,14 +10,19 @@ class Movement
   public:
   enum Properties
   {
-    Speed,
-    TurnAngle,
-    TurnLength,
-
-    Direct_X_Axis,
-    Direct_Y_Axis,
-
-    __COUNT
+    Speed,    ///< The Speed modifier multiplies every movement action. Default
+              /// Value: 1.
+    TurnAngle,///< The TurnAngle is the angle of the arc the rover should follow
+              /// in degrees.
+    TurnLength,   ///< The TurnLength is the length of the arc the rover should
+                  /// follow in meters.
+    Direct_X_Axis,///< This X-Axis represents the X-component of the direct
+                  /// movement vector of the rover. It could be mapped to a
+                  /// joystick. This value is on a 1-1000 scale.
+    Direct_Y_Axis,///< This Y-Axis represents the Y-component of the direct
+                  /// movement vector of the rover. It could be mapped to a
+                  /// joystick. This value is on a 1-1000 scale.
+    __COUNT       ///< The number of properties in this category.
   };
 
   Datatype TypesImpl[(size_t)Properties::__COUNT] = {
@@ -25,8 +30,8 @@ class Movement
     Datatype::DOUBLE,// Movement::TurnAngle,
     Datatype::FLOAT, // Movement::TurnLength
 
-    Datatype::INT_32, // Movement::Direct_X_Axis
-    Datatype::INT_32, // Movement::Direct_Y_Axis
+    Datatype::INT_8,// Movement::Direct_X_Axis
+    Datatype::INT_8,// Movement::Direct_Y_Axis
   };
 
   static Datatype* Types;
