3485f326e201b612bb732a8efe2aa94ee319bee1
Max Heisinger <mail@maximaximal.com>
2018-03-22 18:39:39 +0100

    Add some documentation to DBPersistencyPolicy.

--- RRegistry/include/RRegistry/DBPersistencyPolicy.hpp
+++ RRegistry/include/RRegistry/DBPersistencyPolicy.hpp
@@ -19,6 +19,19 @@ namespace rcomm {
 struct LiteCommMessage;
 }
 namespace rregistry {
+
+/**
+ * @brief Implements persistent saving of registry data into a SQLite database.
+ *
+ * Per default, all database files are captured in ~/public_html with the current
+ * time as filename. Capturing can be activated by enable() and disable() and is
+ * integrated into the LiteCommAdapter to be activated when Bool::PERS_ENABLE is
+ * activated.
+ *
+ * The database can later be queried with commands like this (in SQL):
+ * SELECT general.key,setsInt16.timestamp,setsInt16.value FROM setsInt16
+ *  INNER JOIN general ON general.value = setsInt16.property;
+ */
 class DBPersistencyPolicy : public PersistencyPolicy
 {
   public:
